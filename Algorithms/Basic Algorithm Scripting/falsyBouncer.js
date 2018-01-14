c964002841d6b36cdbc1446831c409be12b9b64d
Omar Lozada <omrlozada@gmail.com>
2018-01-15 07:49:12 +0800

    Create falsyBouncer.js

---
+++ Algorithms/Basic Algorithm Scripting/falsyBouncer.js
@@ -0,0 +1,13 @@
+
+function bouncer(arr) {
+  // Don't show a false ID to this bouncer.
+  newArr = [];
+  arr.filter(function(val, index) {
+    if (val) {
+      newArr.push(val);
+    }
+  });
+  return newArr;
+}
+
+bouncer([7, "ate", "", false, 9]);

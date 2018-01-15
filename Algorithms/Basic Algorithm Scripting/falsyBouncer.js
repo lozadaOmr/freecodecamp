9751258f0e088e2dfe5258e1e0d522612c45174b
Omar Lozada <omrlozada@gmail.com>
2018-01-15 09:02:53 +0800

    Remove unused index argument

--- Algorithms/Basic Algorithm Scripting/falsyBouncer.js
+++ Algorithms/Basic Algorithm Scripting/falsyBouncer.js
@@ -2,7 +2,7 @@
 function bouncer(arr) {
   // Don't show a false ID to this bouncer.
   newArr = [];
-  arr.filter(function(val, index) {
+  arr.filter(function(val) {
     if (val) {
       newArr.push(val);
     }

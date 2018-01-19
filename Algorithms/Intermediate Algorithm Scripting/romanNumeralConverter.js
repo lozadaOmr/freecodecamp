111bdb3d41e7fcb73ad9e218c9678b17ed3e58fd
Omar Lozada <omrlozada@gmail.com>
2018-01-19 21:59:07 +0800

    clean up romanNumeralConverter.js

--- Algorithms/Intermediate Algorithm Scripting/romanNumeralConverter.js
+++ Algorithms/Intermediate Algorithm Scripting/romanNumeralConverter.js
@@ -3,14 +3,12 @@ function convertToRoman(num) {
   var romanArr = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
   var decimalArr = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
 
-  var numStr = num.toString().split("");
   var romanNums = [];
   for (i = decimalArr.length - 1; i >= 0; i--) {
     var currentNum = num/decimalArr[i]; 
     var checker = Math.floor(currentNum) * decimalArr[i];
     
     if (checker > 0) {
-      console.log(num + '---' + checker + '->' + decimalArr[i]);
       romanNums.push(romanArr[i].repeat(Math.floor(currentNum)));
       num -= checker;
     }
